e1854caeb160e0647e46e563f60783be790bf3cf
Erik Butcher <butcer0@gmail.com>
2018-06-08 16:46:13 -0400

    introduced pipetransform for search filtering

--- src/app/search-artists.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SearchArtistsPipe } from './search-artists.pipe';
-
-describe('SearchArtistsPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SearchArtistsPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
